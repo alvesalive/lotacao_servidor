c91fa1414ace55dbe6043a9fa1a7c4307adc1c4c
Willyan Alves <willyan.net@gmail.com>
2025-04-08 22:39:10 -0400

    start project

---
+++ src/main/java/br/com/willyan/seplag/lotacao/LotacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.willyan.seplag.lotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/willyan/seplag/lotacao/LotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.willyan.seplag.lotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
